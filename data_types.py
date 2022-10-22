34403c146478cefbea1694100b2039ee94feb58d
VasiliyKozar <vasilkozar95@gmail.com>
2022-10-22 15:21:00 +0300

    my second homework

---
+++ data_types.py
@@ -0,0 +1,33 @@
+my_age = 27
+print(my_age)
+
+new_car = "Jeep"
+print(new_car)
+
+data_interview = "13 march"
+print(data_interview)
+
+avg_age = 20
+print(avg_age)
+
+number_of_case = 135
+print(number_of_case)
+
+love_season = "summer"
+print(love_season)
+
+name_of_student = "Ilya"
+print(name_of_student)
+
+position = "goalkeeper"
+print(position)
+
+number1 = 15
+print(number1)
+
+left_corner = 4
+print(left_corner)
+
+
+
+
