656f8fbf9410c67164a7e4ddcf83e586e9492dbc
VasiliyKozar <vasilkozar95@gmail.com>
2022-10-30 16:27:39 +0200

    my second homework

--- boll_type.py
+++ boll_type.py
@@ -7,29 +7,26 @@ print(fix)
 comprasions1 = 5 == 5
 print(comprasions1)
 
-comprasions2 = 5<=5
+comprasions2 = 5 <= 5
 print(comprasions2)
 
-comprasions3 = 5>=5
+comprasions3 = 5 >= 5
 print(comprasions3)
 
 # 3. Five combination
-# or
-is_true1 = (4 > 3 + 0) or (3 < 5-1)
+is_true1 = (4 > 3 + 0) or (3 < 5-1) or (1<0)
 print(is_true1)
 
-is_true2 = (1 > 1 - 2 ) or (3 + 5 < 10)
+is_true2 = (1 > 1 - 2 ) or (3 + 5 < 10) and (1 < 0)
 print(is_true2)
 
-# and
-is_true3 = (2 > 1 - 1) and (0 > 0 - 1)
+is_true3 = (2 > 1 - 1) or (10 > 0 - 1) and (1 == 10)
 print(is_true3)
 
-is_true4 = (1 + 1 > 0) and (2 + 2 < 10)
+is_true4 = (1 + 1 > 0) and (2 + 2 < 10) or -(-2) > 3
 print(is_true4)
 
-# not
-nicht = not False -(-2) > 3
+nicht = not False -(-2) < 3 or (2 > 1) or (1 < 0)
 print(nicht)
 
 # 4. logical values
